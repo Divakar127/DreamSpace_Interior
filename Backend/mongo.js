4e9a7fede9b74cebc8a17804145dc8e0cdd2ed74
Divakar127 <Divakarthangavel@gmail.com>
2024-07-22 14:27:58 +0530

    base update

---
+++ Backend/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/Homie")
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+const newSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const collection = mongoose.model("collection", newSchema);
+
+module.exports = collection;
